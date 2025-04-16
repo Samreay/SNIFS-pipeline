8e1dd2bd97fdc0c70e8b4bed719dc032061d35cd
Samuel Hinton <samuelreay@gmail.com>
2025-04-16 14:48:04 +1000

    Cleaning up

--- src/pipeline/tasks/snifs_run_logs.py
+++ src/pipeline/tasks/snifs_run_logs.py
@@ -11,20 +11,13 @@ def parse_snifs_run_logs(resolver: Resolver):
     raw_logs_path = resolver.get_match_path(FileType.RAW_LOGS)
     regex = re.compile(r"SNIFS_on \d \d (.) ==>(.*)\s+$")
     lines = raw_logs_path.read_text(encoding="UTF-8", errors="replace").splitlines()
+    lines = [line for line in lines if "SNIFS_on" in line]
     results = []
     for line in lines:
-        if "SNIFS_on" in line:
-            match = regex.search(line)
-            if match:
-                channel = match.group(1)
-                time_str = match.group(2).strip()
-                time = dt.strptime(time_str, "%a %b %d %H:%M:%S %Z %Y")
-                results.append(
-                    {
-                        "channel": channel,
-                        "time": time,
-                    }
-                )
+        if match := regex.search(line):
+            channel, time_str = match.group(1), match.group(2)
+            results.append(dict(channel=channel, time=dt.strptime(time_str, "%a %b %d %H:%M:%S %Z %Y")))
+
     output_path = resolver.processed_data_path / f"type={FileType.CCD_ON_TIMES.value}" / "times.parquet"
     output_path.parent.mkdir(parents=True, exist_ok=True)
     df = pl.DataFrame(results).sort("time").with_columns(DATETIME_CONVERSION_EXPR)
